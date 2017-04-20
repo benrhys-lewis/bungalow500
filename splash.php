8c595988ae36e863cd447cce0da4529dd69e0bcf
benrhys-lewis <bengyman161@gmail.com>
2017-04-20 23:41:11 +0100

    Create splash.php

---
+++ splash.php
@@ -0,0 +1,26 @@
+<div class="splash">
+	<div class="inner">
+		<img class="logo" src="img/logo.png" />
+		<h1>New boutique B&amp;B<br/>opening 1st June 2017</h1>
+		<a href="https://www.facebook.com/thebungalowullapool/"><img style="margin:20px 20px 0 65px;vertical-align:top;" src="img/fb.png" /></a>
+		<img style="float:right;margin:-40px 20px 0 20px;" width="300px" src="img/home.jpg" />
+		<ul style="list-style:none;text-align:center;margin:-10px 0px 0 -20px;">
+			<li>- 5 min walk to Ullapool town centre -</li>
+			<li>- Large ensuite-rooms with comfy sofas -</li>
+			<li>- Flax linen bedding -</li>
+			<li>- Guest lounge -</li>
+			<li>- In-house sauna -</li>
+			<li>- Private off-road parking -</li>
+			<li>- Fast wi-fi -</li>
+			<li>- Fabulous views -</li>
+			<li>- Woodland garden and terrace to enjoy -</li>
+			<li>- Scrumptious breakfasts -</li>
+		</ul>
+
+		<p style="margin:10px 0px 0 30px;text-align:center;">Stylish and relaxing accommodation for a great stay in this fabulous and unique Highland setting, a ferry ride from the Outer Hebrides</p>
+
+		<p style="margin:20px 0 0 30px;text-align:center;"><strong>Contact details:</strong><br/>
+		Phillip and Elizabeth Pennycott<br/>
+		Tel: 01854613193 / 07752719791<br/>
+		<a href="mailto:bungalow500ullapool@gmail.com">bungalow500ullapool@gmail.com</a></p>
+	</div></div>
