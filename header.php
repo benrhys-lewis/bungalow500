5e54ec4b50edd21c3374a96a27c1d412e6db2e6c
benrhys-lewis <bengyman161@gmail.com>
2017-04-20 22:05:19 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<!doctype html><html>
+<head>
+	<title>Bungalow 500</title>
+	<link href="https://fonts.googleapis.com/css?family=Oxygen|Ropa+Sans" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
